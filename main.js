a9ea7ee3ca7cbbf9f207f7c1e1ec0f31cdd28ea5
Phuong <phuong.zorum24@student.passerellesnumeriques.org>
2023-07-26 14:17:00 +0700

    slider part

--- main.js
+++ main.js
@@ -97,5 +97,71 @@
 
 
 
-// Bìa coffe
+//// Bìa coffe
 
+
+document.addEventListener('alpine:init', () => {
+    Alpine.data('slider', () => ({
+        currentIndex: 1,
+        images: [
+            // 'https://source.unsplash.com/1600x900/?beach',
+            // 'https://source.unsplash.com/1600x900/?cat',
+            // 'https://source.unsplash.com/1600x900/?dog',
+            // 'https://source.unsplash.com/1600x900/?lego',
+            // 'https://source.unsplash.com/1600x900/?textures&patterns'
+        ],
+        back() {
+            if (this.currentIndex > 1) {
+                this.currentIndex = this.currentIndex - 1;
+            }
+        },
+        next() {
+            if (this.currentIndex < this.images.length) {
+                this.currentIndex = this.currentIndex + 1;
+            } else if (this.currentIndex <= this.images.length){
+                this.currentIndex = this.images.length - this.currentIndex + 1
+            }
+        },
+    }))
+});
+let currentSlideID = 1;
+const sliderE = document.getElementById('slider');
+const totalSlides = sliderE.childElementCount;
+console.log(totalSlides);
+
+function next() {
+  if (totalSlides > currentSlideID) {
+    currentSlideID++;
+    showSlide();
+  } else {
+    currentSlideID = 1; // Quay lại slide đầu tiên
+    showSlide();
+  }
+}
+
+function prev() {
+  if (currentSlideID > 1) {
+    currentSlideID--;
+    showSlide();
+  } else {
+    currentSlideID = totalSlides; // Quay lại slide cuối cùng
+    showSlide();
+  }
+}
+
+function showSlide() {
+  const slides = document.getElementById('slider').getElementsByTagName('li');
+  for (let index = 0; index < totalSlides; index++) {
+    const element = slides[index];
+    if (currentSlideID === index + 1) {
+      element.classList.remove('hidden');
+    } else {
+      element.classList.add('hidden');
+    }
+  }
+}
+
+// Chuyển tiếp tự động sau mỗi 5 giây
+setInterval(() => {
+  next();
+}, 5000);
